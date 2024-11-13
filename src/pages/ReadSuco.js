0ffb4497227493753cd177ea8158c26c95715635
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2024-11-13 11:48:53 -0300

    reformulação ReadSuco

--- src/pages/ReadSuco.js
+++ src/pages/ReadSuco.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { apiEndpoint } from "../config/constantes";
+import "./css/ReadSuco.css"; 
 
 const ReadSuco = () => {
   const { id } = useParams();
@@ -13,7 +14,6 @@ const ReadSuco = () => {
       axios
         .get(`${apiEndpoint}/suco/with-diagnostico/${id}`)
         .then((res) => {
-          console.log(res.data); // Examine os dados recebidos no console
           const sucoData = res.data[0] || {};
           setSuco(sucoData);
           setDiagnostico(sucoData.diagnostico || {});
@@ -25,31 +25,26 @@ const ReadSuco = () => {
   const imageUrl = `${suco.img1}`;
 
   return (
-    <div>
+    <div className="read-suco-container">
       {Object.keys(suco).length > 0 && (
         <>
-          <h2>{suco.nome}</h2>
+          <h2 className="suco-title">{suco.nome}</h2>
           {suco.img1 && (
             <img
               src={imageUrl}
               alt={suco.nome}
-              style={{
-                width: "100%",
-                height: "auto",
-                margin: "auto",
-                marginTop: "8px",
-                borderRadius: "10px 10px 0 0",
-              }}
+              className="suco-image"
             />
           )}
-          <p>Ingredientes: {suco.ingredientes}</p>
-          <p>Modo de Preparo: {suco.modo_de_preparo}</p>
-          <p>Benefícios: {suco.beneficios}</p>
-
+          <div className="suco-info">
+            <p><strong>Ingredientes:</strong> {suco.ingredientes}</p>
+            <p><strong>Modo de Preparo:</strong> {suco.modo_de_preparo}</p>
+            <p><strong>Benefícios:</strong> {suco.beneficios}</p>
+          </div>
           {diagnostico.nome_da_condicao && (
-            <div key={diagnostico.id}>
+            <div className="diagnostico-container">
               <h3>Diagnóstico Associado</h3>
-              <p>Nome da Condição: {diagnostico.nome_da_condicao}</p>
+              <p><strong>Nome da Condição:</strong> {diagnostico.nome_da_condicao}</p>
             </div>
           )}
         </>
